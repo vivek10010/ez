8917bb52be78c8ef01eada5af9934603d80f8ed6
VIVEK <vivekcms1@gmail.com>
2023-06-01 12:38:48 +0530

    finished splash

---
+++ app/src/main/java/com/example/ez/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.example.ez
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val iv = findViewById<ImageView>(R.id.iv_splash)
+        iv.alpha = 0f
+        iv.animate().setDuration(2500).alpha(1f).withEndAction{
+            val i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
